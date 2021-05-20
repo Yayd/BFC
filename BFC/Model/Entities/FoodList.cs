dbe7273345106e864b723493547150361bbe518b
Alyona <Alyona-gil@mail.ru>
2021-05-20 21:26:45 +0500

    Создание Entity классов

---
+++ BFC/Model/Entities/FoodList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFC.Model.Entities
+{
+    public class FoodList
+    {
+        public virtual int Id { get; protected set; }
+        public virtual string Name { get; set; }
+        public virtual double Proteins { get; set; }
+        public virtual double Fats { get; set; }
+        public virtual double Carbohydrates { get; set; }
+        public virtual int Calories { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
---
+++ BFC/Model/Entities/UserWeight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFC.Model.Entities
+{
+    public class UserWeight
+    {
+        public virtual int Id { protected set; get; }
+        public virtual DateTime Date { set; get; }
+        public virtual double Weight { set; get; }
+    }
+}
