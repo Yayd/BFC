76f38d7e419532ac9ccfb360d21a390a96173a0b
Alyona <Alyona-gil@mail.ru>
2021-05-20 21:27:37 +0500

    Создание маппингов

---
+++ BFC/Model/Entities/Mapping/DailyRateMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFC.Model.Entities.Mapping
+{
+    public class DailyRateMap : ClassMap<DailyRate>
+    {
+        public DailyRateMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Date);
+            Map(x => x.CaloriesPortion);
+
+        }
+    }
+}
---
+++ BFC/Model/Entities/Mapping/UserWeightMap.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFC.Model.Entities.Mapping
+{
+    public class UserWeightMap : ClassMap<UserWeight>
+    {
+        public UserWeightMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Date);
+            Map(x => x.Weight);
+        }
+    }
+}
