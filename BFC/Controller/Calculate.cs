8c3ecb09d11a3d4299307f33f243840550e800e4
HaNJI <henhooky@gmail.com>
2021-05-21 21:05:54 +0500

    Добавлен основной функционал для работы с формой - добавлен модуль Calculate в Controller - добавлен модуль Formula в Model - пока отсутствует подключение к бд - реализована функция расчета суточного объема калорий - реализована функция расчета калорий продукта

---
+++ BFC/Controller/Calculate.cs
@@ -0,0 +1,46 @@
+﻿using BFC.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFC.Controller
+{
+    public static class Calculate
+    {
+        /// <summary>
+        /// Определяет все принятные параметры и вычисляет конечные значения суточного потребления калорий
+        /// </summary>
+        /// <returns>суточное потребление</returns>
+        public static string GetCalculateCalories(bool sex, double age, double weight, double height, Activity activity, Plan plan, EFormula formula)
+        {
+            var coef = PhysicalActivity.GetCoefficient(activity);
+            int preplanResult = formula switch
+            {
+                EFormula.HarrisBenedict => (int)Formula.HarrisBenedict(sex, weight, height, age, coef),
+                EFormula.MifflinSanGeora => (int)Formula.MifflinSanGeora(sex, weight, height, age, coef),
+                _ => throw new Exception("Formula wasn't chosen"),
+            };
+
+            var result = plan switch
+            {
+                Plan.loss => $"{(int)(preplanResult * 0.85)}-{preplanResult}",
+                Plan.maintaining => $"{preplanResult}",
+                Plan.gain => $"{preplanResult}-{(int)(preplanResult * 1.15)}",
+                _ => throw new Exception("Plan wasn't chosen"),
+            };
+
+            return result;
+        }
+
+        /// <summary>
+        /// Расчитывает количество калорий по продукту
+        /// </summary>
+        /// <param name="weight">вес в граммах</param>
+        /// <param name="calories">калорийность на 100 гр.</param>
+        /// <returns></returns>
+        public static string GetProductCalories(double weight, double calories)
+        {
+            return (weight * calories / 100.0).ToString();
+        }
+    }
+}
---
+++ BFC/Model/Formula.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFC.Model
+{
+    public enum EFormula
+    {
+        MifflinSanGeora,
+        HarrisBenedict
+    }
+
+    static class Formula
+    {
+        /// <summary>
+        /// Формула расчета Миффлина - Сан-Жеора
+        /// </summary>
+        /// <param name="sex">пол 0 - жен., 1 - муж.</param>
+        /// <param name="weight">вес в килограммах</param>
+        /// <param name="height">рост в сантиметрах</param>
+        /// <param name="age">возраст в годах</param>
+        /// <param name="coef">коэффициент физической активности</param>
+        /// <returns>необходимое суточное количество калорий</returns>
+        public static double MifflinSanGeora(bool sex, double weight, double height, double age, double coef)
+        {
+            var formula = (10.0 * weight) + (6.25 * height) - (5.0 * age);
+            var result = sex ? formula + 5 : formula - 161;
+
+            result = Min(result, sex);
+
+            return result * coef;
+        }
+
+        /// <summary>
+        /// Формула расчета Харриса-Бенедикта
+        /// </summary>
+        /// <param name="sex">пол 0 - жен., 1 - муж.</param>
+        /// <param name="weight">вес в килограммах</param>
+        /// <param name="height">рост в сантиметрах</param>
+        /// <param name="age">возраст в годах</param>
+        /// <param name="coef">коэффициент физической активности</param>
+        /// <returns>необходимое суточное количество калорий</returns>
+        public static double HarrisBenedict(bool sex, double weight, double height, double age, double coef)
+        {
+            double formula;
+            if (sex)
+            {   //для мужчин
+                formula = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+            }
+            else
+            {   //для женщин
+                formula = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+            }
+
+            var result = Min(formula, sex);
+
+            return result * coef;
+        }
+
+        /// <summary>
+        /// Нельзя снижать количество потребляемых калорий ниже 1200 для женщин и 1400 для мужчин
+        /// </summary>
+        /// <param name="calories">суточная норма калорий</param>
+        /// <param name="sex">пол 0 - жен., 1 - муж.</param>
+        /// <returns></returns>
+        private static double Min(double calories, bool sex)
+        {
+            //пороговое значение калорий в сутки
+            var min = sex ? 1400.0 : 1200.0;
+            //калибровка
+            var result = Math.Max(calories, min);
+            return result;
+        }
+    }
+}
